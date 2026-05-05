c3d6ad5191cb516a4f192b4bd14e02c7b44e8516
chipchippp <114909102+chipchippp@users.noreply.github.com>
2026-05-06 03:04:38 +0700

    test: add UserController integration test using Testcontainers (PostgreSQL)
    - integrate Testcontainers with Spring Boot
    - configure dynamic datasource for PostgreSQL container
    - test create user API with real database

---
+++ identity-service/src/test/java/com/portal/identity_service/controller/UserControllerIntegrationTest.java
@@ -0,0 +1,88 @@
+package com.portal.identity_service.controller;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import com.portal.identity_service.dto.request.UserCreateRequest;
+import com.portal.identity_service.dto.response.UserResponse;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+
+import java.time.LocalDate;
+
+@Slf4j
+@SpringBootTest
+@AutoConfigureMockMvc
+@Testcontainers
+public class UserControllerIntegrationTest {
+    @Container
+    static final PostgreSQLContainer<?> postgreSQLContainer = new PostgreSQLContainer<>("postgres:latest");
+
+    @DynamicPropertySource
+    static void configDataSource(DynamicPropertyRegistry registry) {
+        registry.add("spring.datasource.url", postgreSQLContainer::getJdbcUrl);
+        registry.add("spring.datasource.username", postgreSQLContainer::getUsername);
+        registry.add("spring.datasource.password", postgreSQLContainer::getPassword);
+        registry.add("spring.datasource.driver-class-name", postgreSQLContainer::getDriverClassName);
+        registry.add("spring.jpa.hibernate.ddl-auto", () -> "update");
+    }
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    private UserCreateRequest request;
+    private UserResponse userResponse;
+    private LocalDate dob;
+
+    @BeforeEach
+    void initData() {
+        dob = LocalDate.of(1990, 1, 1);
+        request = UserCreateRequest.builder()
+                .username("loc15")
+                .password("123456")
+                .dateOfBirth(dob)
+                .build();
+
+        userResponse = UserResponse.builder()
+                .id(1L)
+                .username("loc15")
+                .dateOfBirth(dob)
+                .build();
+    }
+
+    @Test
+    @WithMockUser(username = "admin", roles = {"ADMIN"})
+    void createUser_validRequest_success() throws Exception {
+        // Given
+        ObjectMapper objectMapper = new ObjectMapper();
+        objectMapper.registerModule(new JavaTimeModule());
+        String content = objectMapper.writeValueAsString(request);
+
+        // When, Then
+        var response = mockMvc.perform(MockMvcRequestBuilders
+                        .post("/api/v1/users")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(content))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.jsonPath("$.status").value(201))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.message").value("user.add.success"))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.data.username").value(request.getUsername()));
+
+        log.info("Response: {}", response.andReturn().getResponse().getContentAsString());
+    }
+
+
+}
